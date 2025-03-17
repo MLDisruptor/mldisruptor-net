3e198a0ce194fb3d66b6333d8e7833b57312bef5
Caleb D. McElrath <Metaomniliquant@users.noreply.github.com>
2025-03-16 21:25:18 -0500

    refactor: simplify PerformancePredictorFeature initialization
    - Removed unnecessary `using` statements for Disruptor.
    - Updated namespace declaration for improved readability.
    - Eliminated the overloaded `Initialize` method with multiple parameters.
    - Retained a single `Initialize` method that accepts `FeatureOptions<T>`.
    - Directly set `_ringBufferSize` from `options.RingBufferSize` for better configuration handling.
    
    Signed-off-by: Caleb D. McElrath <Metaomniliquant@users.noreply.github.com>

--- src/packages/MLDisruptor.NET.PerformancePredictor/Internal/Feature/PerformancePredictorFeature.cs
+++ src/packages/MLDisruptor.NET.PerformancePredictor/Internal/Feature/PerformancePredictorFeature.cs
@@ -1,7 +1,4 @@
-﻿using Disruptor;
-using Disruptor.Dsl;
-
-namespace MLDisruptor.NET.PerformancePredictor.Internal.Feature
+﻿namespace MLDisruptor.NET.PerformancePredictor.Internal.Feature
 {
     internal class PerformancePredictorFeature : IMLFeature
     {
@@ -11,11 +8,6 @@ namespace MLDisruptor.NET.PerformancePredictor.Internal.Feature
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
         public string Name => nameof(PerformancePredictorFeature);
-
-        public void Initialize<T>(Func<T> eventFactory, int ringBufferSize, TaskScheduler taskScheduler, ProducerType producerType, IWaitStrategy waitStrategy)
-        {
-            _ringBufferSize = ringBufferSize;
-        }
 
         public void Initialize<T>(FeatureOptions<T> options)
         {
